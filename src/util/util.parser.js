dd1bd35a9fdeab7fae5dea26600a254a833424b7
monsieurbadia <contact@monsieurbadia.com>
2020-04-18 01:27:48 +0200

    D2K: it adds parser collection

---
+++ src/util/util.parser.js
@@ -0,0 +1,18 @@
+import { strings } from 'u3s';
+
+/** @public */
+export const parseTextToFirstLetterUpperCase = ( str, insertion ) => {
+
+  const content = str.split( /[-._]/g );
+
+  return content.reduce( ( result, s, i ) => {
+
+    result += strings.toFirstLetterUpperCase( s );
+
+    if ( insertion && i === content.length - 1 ) result += strings.toFirstLetterUpperCase( insertion );
+
+    return result;
+
+  }, '' );
+
+};
