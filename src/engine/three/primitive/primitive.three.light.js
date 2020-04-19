02f9cadef7c3e526e98e8a385d4edbdfef034a2a
monsieurbadia <contact@monsieurbadia.com>
2020-04-19 20:36:29 +0200

    D2K: it uses strings collection from u3s instead using a custom function

--- src/engine/three/primitive/primitive.three.light.js
+++ src/engine/three/primitive/primitive.three.light.js
@@ -1,17 +1,15 @@
+import { strings } from 'u3s';
 import { parseTextToFirstLetterUpperCase } from '=>/util/util.parser';
 
 /**
  * @author monsieurbadia / https://monsieurbadia.com/
  */
 
-/** @private TODO: move it */
-const convert = str => str.split( ' ' ).map( value => window.parseInt( value ) );
-
 /** @oublic */
 export const THREELight = ( ENGINE, parameters ) => {
 
   const instanceName = parseTextToFirstLetterUpperCase( parameters.option.type, 'light' );
-  const light = new ENGINE[ instanceName ]( convert( ...parameters.option.args ) );
+  const light = new ENGINE[ instanceName ]( strings.ToInt( ...parameters.option.args, ' ' ) );
   
   return light;
 
