fe2fb07b6dfce267bddd7ee4c794f825344be1e8
monsieurbadia <contact@monsieurbadia.com>
2020-04-24 02:57:16 +0200

    D2K: it makes a refactoring in three primitve group

--- src/engine/three/primitive/primitive.three.group.js
+++ src/engine/three/primitive/primitive.three.group.js
@@ -1,8 +1,6 @@
-import { strings } from 'u3s';
-
 /**
  * @author monsieurbadia / https://monsieurbadia.com/
  */
 
 /** @public */
-export const THREEGroup = RENDERING_ENGINE => new RENDERING_ENGINE[ strings.toFirstLetterUpperCaseReducer( 'group' ) ]();
+export const THREEGroup = RENDERING_ENGINE => new RENDERING_ENGINE.Group();
