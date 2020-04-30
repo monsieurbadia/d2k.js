dd4717f10b80c18d308519b78fbc65ca474e8f2e
monsieurbadia <contact@monsieurbadia.com>
2020-04-30 04:46:43 +0200

    D2K: it cleans up the three starter

--- src/starter/starter.three.js
+++ src/starter/starter.three.js
@@ -1,6 +1,5 @@
 import { is, merge } from 'u3s';
 import { Creater } from '=>/core';
-import { THREEScene } from '=>/engine/three';
 
 /**
  * @author monsieurbadia / https://monsieurbadia.com/
@@ -9,92 +8,6 @@ import { THREEScene } from '=>/engine/three';
 export const onthreestarter = ( init = {} ) => {
 
   const conf = init;
-
-  const withCamera = ( ...parameters ) => {
-
-    if ( is.empty( conf.camera ) ) {
-
-      conf.camera = {};
-      
-    }
-
-    conf.camera = Creater.createPrimitive( parameters, 'camera', conf.RENDERING_ENGINE );
-
-    return onthreestarter( conf );
-
-  };
-
-  const withLight = ( ...parameters ) => {
-
-    if ( is.empty( conf.light ) ) {
-
-      conf.light = {};
-      
-    }
-
-    conf.light = Creater.createPrimitive( parameters, 'light', conf.RENDERING_ENGINE );
-
-    return onthreestarter( conf );
-
-  };
-
-  const withMesh = ( ...parameters ) => {
-
-    if ( is.empty( conf.mesh ) ) {
-
-      conf.mesh = {};
-      
-    }
-
-    conf.mesh = Creater.createPrimitive( parameters, 'mesh', conf.RENDERING_ENGINE );
-
-    return onthreestarter( conf );
-
-  };
-
-  const withLoader = ( ...parameters ) => {
-
-    if ( is.empty( conf.loader ) ) {
-
-      conf.loader = {};
-      
-    }
-
-    conf.loader = Creater.createPrimitive( parameters, 'loader', conf.RENDERING_ENGINE );
-
-    return onthreestarter( conf );
-
-  };
-
-  const withRenderer = ( ...parameters ) => {
-
-    if ( is.empty( conf.renderer ) ) {
-
-      conf.renderer = {};
-      
-    }
-
-    conf.renderer = Creater.createPrimitive( parameters, 'renderer', conf.RENDERING_ENGINE );
-
-    return onthreestarter( conf );
-
-  };
-
-  const withScene = ( { name, config } ) => {
-
-    if ( is.empty( conf.scene ) ) {
-
-      conf.scene = {};
-
-    }
-
-    const scene = THREEScene( conf.RENDERING_ENGINE, config );
-
-    conf.scene[ name ] = scene;
-
-    return onthreestarter( conf );
-
-  };
 
   const composify = ( { config } ) => {
 
@@ -134,14 +47,14 @@ export const onthreestarter = ( init = {} ) => {
   const value = _ => conf;
 
   return {
-    withCamera,
-    withLight,
-    withLoader,
-    withMesh,
-    withRenderer,
-    withScene,
     composify,
     value,
+    withCamera: Creater.oncreate( { conf, name: 'camera', starter: onthreestarter } ),
+    withLight: Creater.oncreate( { conf, name: 'light', starter: onthreestarter } ),
+    withLoader: Creater.oncreate( { conf, name: 'loader', starter: onthreestarter } ),
+    withMesh: Creater.oncreate( { conf, name: 'mesh', starter: onthreestarter } ),
+    withRenderer: Creater.oncreate( { conf, name: 'renderer', starter: onthreestarter } ),
+    withScene: Creater.oncreate( { conf, name: 'scene', starter: onthreestarter } ),
   };
 
 };
