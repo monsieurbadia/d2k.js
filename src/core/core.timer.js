696714e7e5e691b4438a1b3993ff874552d618dd
monsieurbadia <contact@monsieurbadia.com>
2020-05-03 04:49:27 +0200

    D2K: it uses the threejs clock to the scene layerization

--- src/core/core.timer.js
+++ src/core/core.timer.js
@@ -1,26 +1,71 @@
-import { is } from 'u3s';
-
 /**
  * @see https://raw.githubusercontent.com/mrdoob/three.js/dev/src/core/Clock.js
  */
 
+let autoStart = ( autoStart !== undefined ) ? autoStart : true;
 let startTime = 0;
-let time = 0;
+let oldTime = 0;
+let elapsedTime = 0;
+let running = false;
+
+const start = _ => {
+
+  startTime = ( typeof performance === 'undefined' ? Date : performance ).now();
+  oldTime = startTime;
+  elapsedTime = 0;
+  running = true;
+
+};
+
+const stop = _ => {
+
+  getElapsedTime();
+
+  running = false;
+  autoStart = false;
+
+};
+
+const getElapsedTime = _ => {
+
+  getDelta();
+
+  return elapsedTime;
+
+};
+
+const getDelta = _ => {
+
+  let diff = 0;
+
+  if ( autoStart && ! running ) {
+
+    start();
+
+    return 0;
+
+  }
+
+  if ( running ) {
+
+    let newTime = ( typeof performance === 'undefined' ? Date : performance ).now();
+
+    diff = ( newTime - oldTime ) / 1000;
+    oldTime = newTime;
+
+    elapsedTime += diff;
+
+  }
+
+  return diff;
+
+}
 
 export const Timer = Object.freeze( {
 
-  getDelta: _ => {
-
-    let diff = 0;
-    let currentTime = ( is.exist( window.performance ) ? Date : window.performance ).now();
-
-    diff = ( currentTime - startTime ) / 1000;
-    startTime = currentTime;
-
-    time += diff;
-
-    return diff;
-
-  }
+  start,
+  stop,
+  getElapsedTime,
+  getDelta
 
 } );
