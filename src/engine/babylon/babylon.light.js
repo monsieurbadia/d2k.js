80e8ec631c7e9b0c3dfefcbf66a93debecfbc0f7
monsieurbadia <contact@monsieurbadia.com>
2020-05-03 06:03:09 +0200

    D2K: it enables the light's config to be dynamic

--- src/engine/babylon/babylon.light.js
+++ src/engine/babylon/babylon.light.js
@@ -14,13 +14,32 @@ export const BABYLONLight = ( {
   }
 } = {} ) => {
 
+  const light = {};
   const { scene } = RENDERING_ENGINE.coreData;
   const instanceName = strings.toFirstLetterUpperCaseReducer( config.type, 'light' );
-  const light = new RENDERING_ENGINE[ instanceName ](
-    config.name, new RENDERING_ENGINE.Vector3( ...config.args ), scene
-  );
 
-  return Object.assign( light, {
+  if ( instanceName === 'SpotLight' ) {
+    
+    const [ position = [ 0, 0, 0, ], direction = [ 0, 0, 0 ], angle = 0, exponent = 2 ] = config.args;
+
+    light.current = new RENDERING_ENGINE[ instanceName ](
+      config.name,
+      new RENDERING_ENGINE.Vector3( ...position ),
+      new RENDERING_ENGINE.Vector3( ...direction ),
+      angle,
+      exponent,
+      scene
+    );
+
+  } else {
+
+    light.current = new RENDERING_ENGINE[ instanceName ](
+      config.name, new RENDERING_ENGINE.Vector3( ...config.args ), scene
+    );
+
+  }
+
+  return Object.assign( light.current, {
     ...Event
   } );
 
