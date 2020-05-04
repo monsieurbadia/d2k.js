809036806dac9f18a0772e81922decaebc3ef0d3
monsieurbadia <contact@monsieurbadia.com>
2020-05-04 04:56:15 +0200

    D2K: it adds an example about fetch a glsl scene from a config file

--- src/engine/three/three.shader.js
+++ src/engine/three/three.shader.js
@@ -10,8 +10,8 @@ export const THREEShader = ( {
   RENDERING_ENGINE,
   config = {
     uniforms: {},
-    fragmentShader: '',
-    vertexShader: ''
+    fragmentShader: 'uniform vec2 resolution;\nuniform vec2 time;\nvoid main () {\n\tvec2 st = gl_FragCoord.xy / resolution.xy;\n\tgl_FragColor=vec4(st.x, st.y, 0.0, 1.0);\n}',
+    vertexShader: 'void main () {\n\tgl_Position = vec4( position, 1.0 );\n}'
   }
 } ) => {
 
