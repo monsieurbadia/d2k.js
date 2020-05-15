9baf1cdc75990845a4080335a9132246e2496b83
monsieurbadia <contact@monsieurbadia.com>
2020-05-15 09:41:50 +0200

    D2K: it used the vanilla typeof before changing

--- src/core/core.event.js
+++ src/core/core.event.js
@@ -9,8 +9,8 @@ const makeWarningLog = _ => console.warn( 'argument is not a "function".' );
 
 export const Event = Object.freeze( {
 
-  onloader: f => is.method( f ) ? CALLBACK.loaders.push( f ) : ( _ => makeWarningLog() )(),
-  onresize: f => is.method( f ) ? CALLBACK.resizers.push( f ) : ( _ => makeWarningLog() )(),
-  onrender: f => is.method( f ) ? CALLBACK.renders.push( f ) : ( _ => makeWarningLog() )()
+  onloader: f => typeof( f ) === 'function' ? CALLBACK.loaders.push( f ) : ( _ => makeWarningLog() )(),
+  onresize: f => typeof( f ) === 'function' ? CALLBACK.resizers.push( f ) : ( _ => makeWarningLog() )(),
+  onrender: f => typeof( f ) === 'function' ? CALLBACK.renders.push( f ) : ( _ => makeWarningLog() )()
 
 } );
